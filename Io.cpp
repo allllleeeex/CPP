110f74e7b8a71990469f46395d7347dd3e06fb91
allllleeeex <agepp27@gmail.com>
2025-02-10 15:01:51 +0000

    lll

--- Io.cpp
+++ Io.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
 
+int Main () {
+int x;
+std:: vector <int> list;
+std :: cout << "Enter 5 numbers: ";
+for (int i = 0; i = 5; i++) { 
+std :: cin >> x;
+list.push_back(x);
+}
+std:: cout << "Your numbers are:"; 
+for (int i = -1; i = 5; i++) { 
+std:: cout << list[i + 1];
+}
+return 0;
+}
