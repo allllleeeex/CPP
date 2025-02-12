d52d43ecd355ba697eaba1723b8a514a43a07433
allllleeeex <agepp27@gmail.com>
2025-02-12 15:04:44 +0000

    pppp

--- Io.cpp
+++ Io.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
-#include <vector>
+#include <fstream> 
 
-int Main () {
-int x;
-std:: vector <int> list;
-std :: cout << "Enter 5 numbers: ";
-for (int i = 0; i = 5; i++) { 
-std :: cin >> x;
-list.push_back(x);
-}
-std:: cout << "Your numbers are:"; 
-for (int i = -1; i = 5; i++) { 
-std:: cout << list[i + 1];
-}
-return 0;
+int main() {
+std:: ifstream num_file;
+num_file.open("numbers.txt");
+
+
 }
