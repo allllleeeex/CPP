88cf915ebaedebd60831797c556fc4e35a1b0976
allllleeeex <agepp27@gmail.com>
2025-02-12 15:11:06 +0000

    lll

--- Io.cpp
+++ Io.cpp
@@ -2,8 +2,15 @@
 #include <fstream> 
 
 int main() {
-std:: ifstream num_file;
+int x;
+std::ifstream num_file;
 num_file.open("numbers.txt");
-
+std::getline(num_file, x);
+std::cout << "There are " <<  x << "numbers in the file.";
+for (int i = 0; i < x; i ++) {
+int y;
+std::getline(num_file, y);
+std:: cout << y;
+}
 
 }
